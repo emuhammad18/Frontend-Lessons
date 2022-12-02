6070ade13cdb3390c9cb23033b94b8ae5117afeb
ergashev_M <wwwergashevmuxammad18@gmail.com>
2022-12-02 23:37:35 +0500

    Lesson 24

---
+++ Lesson_24-Javascript/js/main.js
@@ -0,0 +1,111 @@
+//! var
+//? var => variable
+// var user = "Ibrohim";
+// var x = 5;
+// var y = 6;
+// console.log(user);
+
+// var num1 = 10;
+// var num2 = 5;
+// var answer = num1 * num2;
+// console.log(answer); 
+// num1 = 80;
+// console.log(num1 - num2);
+
+// var user_one;
+// var user_1;
+// var userOne;
+
+// var user_one;
+// console.log(user_one);
+// user_one = "Ibrohim";
+// console.log(user_one);
+
+//! let vs const
+// let user = "Ahmadillo";
+// let user;
+// user = "Ibrohim";
+// console.log(user);
+
+// const user_2 = "Abdulaziz";
+// user_2 ="Pang pung";
+// console.log(user_2);
+
+// const price1 = 5;
+// const price2 = 6;
+// let total = price1;
+// total = 20;
+// console.log(total);
+
+// x=5;
+// y=6;
+// console.log(y-x);
+
+// let user1 = "Ibrohim";
+// let user2 = "Malika";
+// let wedding = user1 + " & " + user2;
+// console.log(wedding);
+
+//! Home work
+
+//? Matematical examples 
+// let x = 35;
+// let y = 55;
+// console.log(y * x);
+
+
+
+// var price_1 = 15;
+// var price_2 = 3;
+// var answer = price_1 / price_2;
+// console.log(answer);
+
+// let num_1 = 42;
+// let num_2 = 28;
+// let num_3 = 14;
+// let total = num_1 * num_3 - num_2;
+// console.log(total);
+
+// lalaku = 65;
+// prima = 24;
+// huggies = 38;
+// pampers = 33;
+// kippers = 11;
+// baby_boo = 47;
+// total = lalaku * prima + pampers / kippers - huggies + baby_boo;
+// console.log(total);
+
+// const flash = 12;
+// const redBull = 6;
+// const adrenalinRush = 9;
+// const jaguar = 15;
+// console.log(flash + adrenalinRush * redBull - jaguar);
+
+//? String examples
+
+// let user_1 = "Akobir";
+// let user_2 = " Axmad";
+// console.log(user_1 + user_2);
+
+// let person_1 = "Sulton";
+// let person_2 = "Samira";
+// let answer = person_2 + " " + person_1;
+// console.log(answer);
+
+// var puple1 = "Asror";
+// var puple2 = "Boxodir";
+// var puple3 = "Navruz";
+// console.log(puple1 + " " + puple2 + " " + puple3);
+
+// name = "Navruz";
+// familyName = "Utaganov";
+// nickName = "u_navruz";
+// total = name + familyName + " " + nickName;
+// console.log(total);
+
+// fullName = "Asror Akromov";
+// age = 16;
+// hobbies = "Playing Foodball"
+// birthDay = 7012006;
+// answer = fullName + " " + age + " " + hobbies + " " + birthDay;
+// console.log(answer);
