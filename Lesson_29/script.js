9da613d24a2696d9b02f0b3adf32e321f5f275bc
ergashev_M <wwwergashevmuxammad18@gmail.com>
2022-12-03 22:09:28 +0500

    Lesson 29

---
+++ Lesson_29/script.js
@@ -0,0 +1,50 @@
+//! DOM => Document object modal
+// console.log(document.getElementsByTagName("div"));
+// console.log(document.getElementsByClassName("flesh"));
+// console.log(document.getElementById("pangpung"));
+
+// console.log(document.querySelector("h1"));
+// console.log(document.querySelectorAll('h1')[1] );
+
+//! Amaliyot
+// const element = document.querySelector("#pangpung")
+
+// let = isRed = false;
+
+// element.onclick = function (){
+//     this.style.color = 'red'
+// } 
+
+// const arr = [];
+// const firstname = prompt("Ism")
+// arr.push(firstname);
+// console.log(arr);
+// const surname = prompt("Familiya")
+// arr.push(surname);
+// console.log(arr);
+// const age = +prompt("Yosh");
+// arr.push(age);
+
+// const template = "Ismingiz " + arr[0] + " Familiyangiz " + arr[1] + " Yoshingiz " + arr[2];
+
+// alert (template);
+
+//! Boolen => True & False < / > / == / ===
+// const salary = 1000;
+// const epanses = 1000;
+
+// if(salary > epanses) {   
+//     console.log("Ishga kir idiot!");
+// } else if (salary < epanses){
+//     console.log("Oylikni oshiring aka!");
+// } else{
+//     console.log("ortirolmaymiz aka!");
+// }
+
+// console.log(20 / "Hello"); //?NaN => Not a Number
+
+// if(NaN){
+//     console.log("Hello");
+// } else{
+//     console.log("Bye");
+// }
