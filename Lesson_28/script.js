8d73f55a15cefc2320feb75ad872dbdd51406930
ergashev_M <wwwergashevmuxammad18@gmail.com>
2022-12-03 22:08:50 +0500

    Lesson 28

---
+++ Lesson_28/script.js
@@ -0,0 +1,46 @@
+// function question(){
+//     const myQuestion = prompt("Sizning savolingiz?")
+//     const mainQ = prompt(myQuestion);
+//     console.log(mainQ);
+// }
+
+//! DOM => Document object model
+// window.alert("Hello");
+// console.log(document)
+
+// const window = {
+//     document: {
+//         all:
+//     }
+// }
+// window.document
+
+//! Arrays
+// const user = ["Ibrohim","Samandar", "Muhammad", "Ahmadillo", 20, false,
+//  undefined, null, {name:"Lalaku"}, "Hello"]
+
+//  const username = user[0];
+
+//  user[0] = true;
+//  user["1"] = null;
+
+//  user[user.length] = "Hello 2";
+//  user[user.length] = 39;
+
+//  console.log(user[user.length - 1]);
+//  console.log(user);
+//  console.log(user[1]);
+
+//? Push & Pop
+// const cars = ["BMW", "GM", "WW", "Mercedec Benz"];
+
+// cars.push("Bugatti");
+// console.log(cars);
+// cars.pop();
+// console.log(cars);
+
+//? Shift & Unshift
+// cars.unshift("Hello")
+// console.log(cars);
+// cars.shift();
+// console.log(cars);
