741112c7f09b62843d16f7c03946b43aec28b2ca
ergashev_M <wwwergashevmuxammad18@gmail.com>
2022-12-04 22:09:29 +0500

    Lesson 33

---
+++ Lesson_33/script.js
@@ -0,0 +1,83 @@
+//! Type Coercion and Type Conversion
+//? type Coercion
+
+let birthYear = 2002;
+// console.log("22" + birthYear);
+// let num = 9_000_000;
+// console.log(num);
+// console.log(true + +"1");
+// console.log(false + true);
+// console.log("4" ** 2);
+
+//? Type Conversion
+// console.log(Number("22") + birthYear);
+// console.log(typeof String(true));
+// console.log(Number("Ibrohim"));
+// console.log(Number("88_99"));
+// console.log(Number(""));
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+// console.log((123).toString());
+
+//? Sorting arrays
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits);
+// fruits.sort();
+// console.log(fruits);
+
+// function func(a, b) {
+//     return a - b;
+// };
+
+// const nums = [1, 5, 6, 3, 8, 9, 11, 33, 88, 111,];
+// console.log(nums);
+// nums.sort(func);
+// console.log(nums);
+
+//? revers ()
+// const arr = nums.reverse();
+// console.log(arr, );
+
+// const cars = [
+//     {type: "Volvo", year: 2016},
+//     {type: "Saab", year: 2001},
+//     {type: "BMW", year: 2010},
+// ];
+
+// cars.sort(function(a,b){
+//     return a.year-b.year;
+// });
+
+// console.log(cars);
+
+//? Switch
+// let user = "Ibrohim";
+
+// if(user === "Ibrohim"){
+//     console.log("Hello " + user);
+// }else{
+//     console.log("Hello");
+// };
+
+// let user = "3";
+
+// if(user == 3){
+//     console.log("Hello " + user);
+// }else if(user === 7){
+//     console.log("Hello");
+// };
+
+// let num = 3;
+
+// switch (num) {
+//     case 5:
+//         console.log("Hello world");
+//         break;
+//     case 8:
+//         console.log("Lalaku");
+//         break;
+//     default:
+//         console.log("Last chanse");
+// }
