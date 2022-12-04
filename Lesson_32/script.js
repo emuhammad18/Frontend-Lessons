d24a808347ba34fba2087b98d92bdfcdc177b3e6
ergashev_M <wwwergashevmuxammad18@gmail.com>
2022-12-04 22:08:59 +0500

    Lesson 32

---
+++ Lesson_32/script.js
@@ -0,0 +1,100 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mainbtn = document.getElementById("btn")
+// const nbtn = document.getElementById("btnh")
+// const primary = document.getElementById("header")
+// let task = false;
+
+// mainbtn.onclick = function () {
+//     if (!task) {
+//         primary.innerHTML = "Teacher";
+//         task = true;
+//     } else {
+//         primary.innerHTML = "Student";
+//         task = false;
+//     }
+// }
+
+// nbtn.onclick = function () {
+//     primary.innerHTML = "Hello";
+// }
+
+// const yourmoney = +prompt("Nechpulingiz bor:");
+// const borish = 500;  // Dollarda
+// const hotel = 250;   // Dollarda
+// const museum = 120;  /// Euroda
+
+// const totaldollar = (borish + hotel) * 9433.34;
+// const total = museum * 10354.03 + totaldollar;
+// const totalall = yourmoney - total;
+// const primary = document.getElementById("header")
+
+// function task() {
+//     if (!yourmoney < totalall) {
+//         primary.innerHTML = "Yaxshi dam olib kel Alisher!";
+//     } else if (yourmoney > totalall) {
+//         primary.innerHTML = "Hali bor sal keyin borasan!";
+//     }
+// }
+// task()
+
+
+// const arr = [];
+// const st = "Student" 
+// const us = "Ustoz" 
+// const primary = document.getElementById("header")
+// const firstname = prompt("Ismingizni kiriting:");
+// arr.push(firstname)
+// const age = +prompt("Yoshingizni kiriting:");
+// arr.push(age)
+// const work = Number(prompt(`Kaspingiz nma: 1.${st}, 2.${us}`));
+// if(work == 1){
+//     arr.push(st)
+//     primary.innerHTML = `${arr[0]} ${arr[1]} ${arr[2]}`
+// }else if(work == 2){
+//     arr.push(us)
+//     primary.innerHTML = `${arr[0]} ${arr[1]} ${arr[2]}`
+// }else{
+//     primary.innerHTML= "Hali ish joyingiz yoq!"
+// }
+ 
+ 
