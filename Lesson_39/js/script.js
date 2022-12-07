7b8999b70893b087e56f7d27d1e5c74e8b52efd9
ergashev_M <wwwergashevmuxammad18@gmail.com>
2022-12-07 22:58:08 +0500

    Lesson 39

---
+++ Lesson_39/js/script.js
@@ -0,0 +1,77 @@
+const arr = [2, 4, 5, 6, 7, 7];
+
+//? forEach
+// const newArr = arr.forEach((item, index) => {
+// });
+
+// console.log(newArr);
+
+//? map
+// const newArrMap = arr.map((item, index) => {
+//   return item;
+// });
+
+// console.log(newArrMap);
+
+//? filter
+// const filteredArr = arr.filter((item, index) => {
+//   return item === 7;
+// });
+
+// console.log(filteredArr);
+
+//? reduce
+// const newArr = arr.reduce((prev, curr) =>{
+//     return curr + prev
+// })
+
+// console.log(newArr);
+
+//! Input
+// const array = [
+//   ["name", "Ibrohim"],
+//   ["ism", "Ismoil"],
+//   ["name", "Ahmadillo"],
+// ];
+
+// function arrToObj(arr) {
+//   let newArr = [];
+//   for (let i = 0; i < array.length; i++) {
+//     let obj = { [arr[i][0]]: arr[i][1] };
+//     newArr.push(obj);
+//   }
+//   return newArr;
+// }
+
+// console.log(arrToObj(array));
+
+//! Output
+// [{ name: "Ibrohim" }, { ism: "Ismoil" }, { name: "Ahmadillo" }];
+
+//! kelgan sonni olib 1dan o'sha songacha arrayga kirgizib return qilish kerak edi
+// function numArr (num) {
+//     let arr = []
+//     for (let i = 1 ; i <= num; i++) {
+//         arr.push(i)
+//     }
+//     return arr
+// }
+
+// console.log(numArr(66));
+
+// function numArr(num) {
+//   let arr = [];
+//   for (let i = 1; i < num; i++) {
+//     if (i % 2 === 0) {
+//       arr.push(i);
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log(numArr(30));
+
+// function func(a, b) {
+//   let arr = [];
+//   for (let i = 1; i < a; i++) {}
+// }
